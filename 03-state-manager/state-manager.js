863cdc4b6c16e538b59a1238f113d1c8cc6316a3
Magesh Kuppan <tkmagesh77@gmail.com>
2023-10-13 11:12:53 +0530

    State-Manager - ver2.0

--- 03-state-manager/state-manager.js
+++ 03-state-manager/state-manager.js
@@ -1,7 +1,8 @@
 let StateManager = (()=>{
     let _currentState = undefined,
         _subscriber = undefined,
-        _reducer = undefined
+        _reducer = undefined,
+        _init_action = { type : '@@INIT/ACTION' }
 
     function getState(){
         return _currentState;
@@ -13,7 +14,8 @@ let StateManager = (()=>{
         let newState = _reducer(_currentState, action);
         if (newState === _currentState) return; //NO state changes
         _currentState = newState;
-        _subscriber()
+        if (typeof _subscriber === 'function')
+            _subscriber()
     }
 
     function createStore(reducer){
@@ -21,6 +23,7 @@ let StateManager = (()=>{
             throw new Error('reducer function is mandatory to create a store');
         }
         _reducer = reducer;
+        _currentState = _reducer(undefined, _init_action) // to initialize the store with the default state (much before any action is dispatched)
         return { getState, subscribe, dispatch }
     }
 
